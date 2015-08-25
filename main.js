d40470d6bacd5b8c8f6fa31746069948dca65aa1
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-08-25 22:54:58 +0900

    ssl対応

--- main.js
+++ main.js
@@ -27,29 +27,38 @@ chrome.sockets.tcp.onReceive.addListener(function(info) {
 		var socketIdToBrowser = socketIdMapServerResponse2Browser[socketId];
 		console.debug("サーバー→ブラウザ: ", socketId, "→", socketIdToBrowser);
 		chrome.sockets.tcp.send(socketIdToBrowser, info.data, function(info) {
-			
+			console.debug("[送信完了] サーバー→ブラウザ: ", socketId, "→", socketIdToBrowser);
 		});
 	} else {
 		// ブラウザ→サーバー
-		isRequestFromBrowser[socketId] = true;
-		var requestTextArray = arrayBuffer2string(info.data).split(MESSAGE_SEPARATOR);
-		var firstLine = requestTextArray[0];
-		if (firstLine.match(/^(GET|POST) (http:\/\/([^/]+)(\/[^ ]+)) (.*)$/i)) {
-			var method = RegExp.$1;
-			var url = RegExp.$2;
-			var host = RegExp.$3;
-			var path = RegExp.$4;
-			var other = RegExp.$5;
-			console.log(socketId, "Request: ", url);
-			requestTextArray[0] = method + " " + path + " " + other;
-			var arrayBuffer = string2arrayBuffer(requestTextArray.join(MESSAGE_SEPARATOR));
-			serverRequest(socketId, host, arrayBuffer);
-		} else if (firstLine.match(/^CONNECT ([^ ]+) (.*)$/i)) {
-			var host = RegExp.$1;
-			var other = RegExp.$2;
-			console.log(socketId, "SSL CONNECT: ", host)
+		if (isSslRequestFromBrowser[socketId]) {
+			// SSL
+			console.debug("SSLブラウザ→サーバー", socketId, sslConnectionSocketIdMap[socketId]);
+			chrome.sockets.tcp.send(sslConnectionSocketIdMap[socketId], info.data, function(info) {
+				
+			});
 		} else {
-			console.warn(socketId, "非対応プロトコル: ", requestTextArray)
+			isRequestFromBrowser[socketId] = true;
+			var requestTextArray = arrayBuffer2string(info.data).split(MESSAGE_SEPARATOR);
+			var firstLine = requestTextArray[0];
+			if (firstLine.match(/^(GET|POST) (http:\/\/([^/]+)(\/[^ ]+)) (.*)$/i)) {
+				var method = RegExp.$1;
+				var url = RegExp.$2;
+				var host = RegExp.$3;
+				var path = RegExp.$4;
+				var other = RegExp.$5;
+				console.log(socketId, "Request: ", url);
+				requestTextArray[0] = method + " " + path + " " + other;
+				var arrayBuffer = string2arrayBuffer(requestTextArray.join(MESSAGE_SEPARATOR));
+				requestToServer(socketId, host, arrayBuffer);
+			} else if (firstLine.match(/^CONNECT ([^ ]+) (.*)$/i)) {
+				var host = RegExp.$1;
+				var other = RegExp.$2;
+				console.log(socketId, "SSL CONNECT: ", host);
+				connectToServer(socketId, host);
+			} else {
+				console.warn(socketId, "非対応プロトコル: ", requestTextArray)
+			}
 		}
 	}
 });
@@ -77,13 +86,14 @@ chrome.sockets.tcp.onReceiveError.addListener(function(info) {
 					if (serverRequestInfo.socketId === socketId) {
 						serverRequestsInfo.splice(i, 1);
 						if (len === 1) {
+							// TODO: ↓の処理が不完全。メッセージを受信する前にcloseする場合あり
 							window.setTimeout(function (){
 								if (isRequestFromBrowser[socketId] && serverRequestsInfoMap[socketIdFromBrowser].length === 0) {
 									chrome.sockets.tcp.close(socketIdFromBrowser);
 									delete isRequestFromBrowser[socketId];
 									delete serverRequestsInfoMap[socketIdFromBrowser];
 								}
-							}, 100);
+							}, 2000);
 						}
 						break;
 					}
@@ -96,8 +106,14 @@ chrome.sockets.tcp.onReceiveError.addListener(function(info) {
 				});
 				delete serverRequestsInfoMap[socketId];
 				console.debug("通信終了（ブラウザ→サーバー）", socketId);
+			} else if (isSslRequestFromBrowser[socketId]) {
+				// SSL通信：ブラウザ→サーバー
+				console.log("切断：SSL通信：ブラウザ→サーバー");
+				delete isSslRequestFromBrowser[socketId];
+				delete sslConnectionSocketIdMap[socketId];
 			} else {
-				console.error("通信エラー（得体のしれない）", info);
+				// たまに検知できてない通信の切断を検知する
+				// console.warn("通信エラー（得体のしれない：たまにsocketIdが飛ぶ）", info);
 			}
 			break;
 		default:
@@ -111,7 +127,7 @@ var isRequestFromServer = {};
 var socketIdMapServerResponse2Browser = {};
 var serverRequestsInfoMap = {};
 
-function serverRequest(socketIdFromBrowser, host_port, arrayBuffer) {
+function requestToServer(socketIdFromBrowser, host_port, arrayBuffer) {
 	var serverRequestsInfo = serverRequestsInfoMap[socketIdFromBrowser] || [];
 	for (var i = 0, len = serverRequestsInfo.length; i < len; i++) {
 		var serverRequestInfo = serverRequestsInfo[i];
@@ -141,9 +157,42 @@ function serverRequest(socketIdFromBrowser, host_port, arrayBuffer) {
 		});
 			
 		chrome.sockets.tcp.connect(socketId, host, port, function(result) {
-			chrome.sockets.tcp.send(socketId, arrayBuffer, function(info) {
-				console.debug("ブラウザ→サーバー: ", host_port, ": ", socketIdFromBrowser, "→", socketId);
-			});
+			if (result < 0) {
+				console.warn("chrome.sockets.tcp.connectエラー: ", result, host, port);
+			} else {
+				chrome.sockets.tcp.send(socketId, arrayBuffer, function(info) {
+					console.debug("ブラウザ→サーバー: ", host_port, ": ", socketIdFromBrowser, "→", socketId);
+				});
+			}
+		});
+	});
+}
+
+var isSslRequestFromBrowser = {};
+var sslConnectionSocketIdMap = {};
+function connectToServer(socketIdFromBrowser, host_port) {
+	// SSL通信の場合はコネクションを張るだけ
+	var temp = host_port.split(":");
+	var host = temp[0];
+	var port = parseInt(temp[1] || 80);
+	chrome.sockets.tcp.create(function(createInfo) {
+		var socketId = createInfo.socketId;
+		isRequestFromServer[socketId] = true;
+		socketIdMapServerResponse2Browser[socketId] = socketIdFromBrowser;
+		
+		isSslRequestFromBrowser[socketIdFromBrowser] = true;
+		sslConnectionSocketIdMap[socketIdFromBrowser] = socketId;
+			
+		chrome.sockets.tcp.connect(socketId, host, port, function(result) {
+			if (result < 0) {
+				console.warn("chrome.sockets.tcp.connectエラー: ", result, host, port);
+			} else {
+				var responseTextArray = ["HTTP/1.1 200 Connection established", "Connection: close", "", ""];
+				var arrayBuffer = string2arrayBuffer(responseTextArray.join(MESSAGE_SEPARATOR));
+				chrome.sockets.tcp.send(socketIdFromBrowser, arrayBuffer, function(info) {
+					console.debug(socketId, "プロキシ→ブラウザ: コネクション確立", host_port);
+				});
+			}
 		});
 	});
 }
